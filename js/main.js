4e23082bdf8e2918e648cdecb8590608f3978750
Kay Chetty <Kay Chetty>
2016-09-15 23:30:16 +1200

    Add project files and folders

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('div#plugin1').dblclick(function() {
+        $(this).fadeOut('fast');
+    });
+});
+   
+
+$("button").click(function(){
+    $.getJSON("http://api.fixer.io/latest", function(result){
+        $.each(result, function(i, field){
+            $("div#output").append(field + " ");
+        });
+    });
+});
+
+$('div#plugin2').dbclick(function() {
+        $(this).fadeOut('fast');
+    });
+
