b57b743ba388cc454aa79239adbe439daaf115aa
Kay280 <kche584@aucklanduni.ac.nz>
2016-09-16 00:50:22 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 $("button").click(function(){
     $.getJSON("http://api.fixer.io/latest", function(result){
-        $.each(result, function(i, field){
+        $.each(result, function(field){
             $("div#output").append(field + " ");
         });
     });
