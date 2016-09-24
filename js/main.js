9d4bdd19f6bb3ce120167e7f91fe0f461d270558
Kay280 <kche584@aucklanduni.ac.nz>
2016-09-24 21:09:19 +1200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-$(document).ready(function() {
-    $('#plugin1').dblclick(function() {
+$(document).ready(function () {
+    $('#plugin1').dblclick(function () {
         $(this).fadeOut('fast');
     });
-    
-    $('#plugin2').hover(function() {
+    $('#plugin2').hover(function () {
         $(this).fadeOut('fast');
     });
-    
+    $('.pull-me').click(function () {
+        $('.panel').slideToggle('slow');
+    })
 });
-
-   
-
-$("button").click(function(){
+/*$("button").click(function(){
     $.getJSON("http://api.fixer.io/latest", function(result){
         console.log(result);
         $.each(result.rates, function(i, field){
@@ -19,4 +17,41 @@ $("button").click(function(){
             $("div#output").append(i + " " + field + " ");
         });
     });
+});*/
+var outputArea = $("div#output");
+var allRates = $('#all');
+$("#b1").click(function () {
+    $.ajax({
+        type: "GET",
+        url: "http://api.fixer.io/latest",
+        success: function (data) {
+            //console.log("success", data);
+            $.each(data.rates, function (i, item) {
+                allRates.append(i + " " + item + " ");
+                if(i === "NZD"){
+                    outputArea.append(i + " " + item);
+                }
+            });
+        }
+    });
+});
+
+$("#search").on("click", function(){
+  var searchTerm = $("#searchTerm").val();
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?";
+  $.ajax({
+    url: url,
+    type: "GET",
+    async: false,
+    dataType: "json",
+    success: function(data, status, jqXHR){
+      // console.log(data);
+      for(var i = 0; i < data[1].length; i++) {
+        $("#wikiOutput").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
+        
+      }
+    }
+    
+  });
+  
 });
